da30c929e64fa0afb8d4b132ae8f0bc6197ee056
Diego Lopez <diegolopezuncc@gmail.com>
2024-07-21 21:11:32 -0400

    feat: Add SPY data and web app files
    The commit adds the SPY data file "SPY_4-21-2024_to_7-21-2024.csv" and the web app files "app.py" and "dataCleaner.py".

---
+++ app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+from dataCleaner import data
+
+# Set the title of the web app
+st.title('SPY Linear Regression Model for 4/21/2024 to 7/21/2024')
+
+# Display the data
+st.write(data)
+
+
---
+++ dataCleaner.py
@@ -0,0 +1,7 @@
+import numpy as np
+import pandas as pd
+
+# Read in the data
+data = pd.read_csv("SPY/SPY_4-21-2024_to_7-21-2024.csv")
+
+
